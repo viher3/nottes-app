8b711ddda5053740ff067ae00721b740ae97885b
viher3 <viher_3@hotmail.com>
2019-01-13 04:36:51 +0100

    Added logout button in the dashboard component

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { Container, Content, Header, Left, Body, Right, Button, Icon, Title, Text } from 'native-base';
+import { AsyncStorage } from "react-native";
 
 export default class Dashboard extends React.Component 
 {
+	constructor(props)
+	{
+		super(props);
+	}
+
+	logout = async () => 
+	{
+		try 
+		{
+			await AsyncStorage.removeItem('userToken');
+
+			this.props.navigation.navigate('Login');
+
+			console.log("logout click");
+		}
+		catch (error) 
+		{
+			// Error saving data
+		    console.log(error);
+		}
+	}
+
 	render() 
 	{
 	    return (
@@ -19,6 +42,11 @@ export default class Dashboard extends React.Component
         	  		</Body>
         	  		<Right />
     	      	</Header>
+
+    			<Button full onPress={() => this.logout()} style={{ marginTop: 50 }}>
+        			<Text>Logout</Text>
+      			</Button>
+
           	</Container>
           	
 	    );
